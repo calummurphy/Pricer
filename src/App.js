1f9a1725a5cadfcb574a8d794518ccc3019046ba
calummurphy <56669700+calummurphy@users.noreply.github.com>
2021-02-03 17:39:04 +0200

    UI and styling updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import PriceList from './components/PriceList'
 import NewCurrencyForm from './components/CurrencyForm';
 import {useState} from 'react'
-import ReactLogo from './PRICER logo svg.svg';
+import ReactLogo from './Pricer written logo.svg';
 
 
 
@@ -11,18 +11,27 @@ function App() {
 
   return (
     <div className="App">      
-   
       <header className="App-header">
-        
-        <h1 className = "Heading" ><img src={ReactLogo} alt="Logo" />  PRICER </h1>
-        <div>
+
+        <h1 className = "Heading" >
+          <div className = 'logo'>
+            <img  src={ReactLogo} alt="Logo" />
+          </div>
+        </h1>
+
+        {/* <div>
           <p className = 'instructions-LABEL'>ENTER CURRENCY TICKER: </p>
-          <form className = 'CurrencySearch'>
+        </div> */}
+
+        <div className = 'CurrencySearch'>
           <NewCurrencyForm></NewCurrencyForm>
           {/* <PriceList></PriceList> */}
-          </form>
+        </div>
+
+        <div className = 'BottomBlock'>
 
         </div>
+
       </header>
     </div>
   );
